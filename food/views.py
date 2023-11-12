6e3847de7ee5fc11ac795e8a1ffba247abd8287c
DevAro178 <me.ammad1786@gmail.com>
2023-11-12 18:36:24 +0500

    Lecture 57

--- food/views.py
+++ food/views.py
@@ -3,6 +3,7 @@ from django.test import TestCase
 from django.http import HttpResponse
 from .models import Item
 from .forms import ItemForm
+from django.views.generic.list import ListView
 
 
 # Create your tests here.
@@ -10,6 +11,12 @@ def index(request):
     item_list = Item.objects.all()
     context = {"item_list": item_list}
     return render(request, "food/index.html", context)
+
+
+class IndexClassView(ListView):
+    model = Item
+    template_name = "food/index.html"
+    context_object_name = "item_list"
 
 
 def detail(request, item_id):
